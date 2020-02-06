8dca0400f8e0261a1a82e2689d6baa9ddb925bcc
erin <nire9221@gmail.com>
2020-02-06 14:35:40 -0800

    fixes bug

--- src/Assignment5_2.java
+++ src/Assignment5_2.java
@@ -7,43 +7,49 @@
 //*******************************************************************
 
 import java.util.Scanner;
+
 public class Assignment5_2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner console = new Scanner(System.in);
-		PrintGPA(console);
-		
-	}
-	public static void PrintGPA(Scanner console) {
-		
+        Scanner console = new Scanner(System.in);
+        PrintGPA(console);
+
+    }
+
+    public static void PrintGPA(Scanner console) {
+
         System.out.print("Enter a student record? ");
-        
+
         String name = console.next();
+        System.out.println(name);
         int num = console.nextInt();
-        
-        int sum = 0;
-        //int count = 0;
-        
-        for (int i = 0; i <= num; i++)
-        {
+        System.out.println(num);
+        double sum = 0;
+        // int count = 0;
+
+        for (int i = 0; i < num; i++) {
+
             sum += console.nextInt();
-            //count++;
+            System.out.println(sum);
+            // count++;
         }
-        double gpa = (double) sum / num;
+        double gpa = sum / num;
+        System.out.println(gpa);
         System.out.println(name + "'s grade is " + gpa);
-     }
+    }
 }
 
+// Write a method called PrintGPA that accepts a Scanner for the console as a
+// parameter and
+// calculates a student's grade point average. The user will type a line of
+// input containing the
+// student's name, then a number that represents the number of scores, followed
+// by that many
+// integer scores. here are two example dialogues:
 
-
-//Write a method called PrintGPA that accepts a Scanner for the console as a parameter and 
-//calculates a student's grade point average. The user will type a line of input containing the
-//student's name, then a number that represents the number of scores, followed by that many
-//integer scores. here are two example dialogues:
-
-//	Enter a student record: Maria 5 72 91 84 89 78
-//	Maria's grade is 82.8 
-//	
-//	Enter a student record: Jordan 4 86 71 62 90
-//	Jordan's grade is 77.25
+// Enter a student record: Maria 5 72 91 84 89 78
+// Maria's grade is 82.8
+//
+// Enter a student record: Jordan 4 86 71 62 90
+// Jordan's grade is 77.25
